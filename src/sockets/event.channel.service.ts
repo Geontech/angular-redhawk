7cd68879cc7b561506c05d85c3f6be2ffb416de5
Thomas Goodwin <btgoodwin@geontech.com>
2016-11-07 13:37:37 -0500

    Added RhMessage to model the MessageEvent type that is common on event channels

--- src/sockets/event.channel.service.ts
+++ src/sockets/event.channel.service.ts
@@ -15,6 +15,8 @@ import { JsonSocketService } from './json.socket.service';
 import { EventSocketUrl } from '../shared/config.service';
 import { IEventChannelCommand } from './event.channel.command';
 
+import { RhMessage } from './message/message';
+
 /**
  * The EventChannelService is a basic event channel interface.  You can
  * subscribe to any channel (WebSocket topics) based on domain ID.
@@ -25,10 +27,10 @@ export class EventChannelService {
     /**
      * All events coming from this web socket
      */
-    public get events$(): Observable<any> { return this.events.asObservable(); }
+    public get events$(): Observable<any|RhMessage> { return this.events.asObservable(); }
 
     // All events and "send" interface
-    private events: Subject<any|IEventChannelCommand>;
+    private events: Subject<any|RhMessage|IEventChannelCommand>;
 
     /**
      * Connects to the named event channel per the provided domain ID
@@ -71,10 +73,14 @@ export class EventChannelService {
             let injector = ReflectiveInjector.resolveAndCreate([JsonSocketService]);
             jsonSocket = injector.get(JsonSocketService);
         }
-        this.events = <Subject<any>> jsonSocket
+        this.events = <Subject<any|RhMessage>> jsonSocket
             .connect(EventSocketUrl())
-            .map((response: MessageEvent): any => {
-                return JSON.parse(response.data);
+            .map((response: MessageEvent): (any | RhMessage) => {
+                let data: any = JSON.parse(response.data);
+                if (data.hasOwnProperty('id') && data.hasOwnProperty('value')) {
+                    return new RhMessage().deserialize(data);
+                }
+                return data;
             });
     }
 }
