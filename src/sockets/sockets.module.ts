70c53b446198e782007c8e9512e5ce4275c5906a
Thomas Goodwin <btgoodwin@geontech.com>
2016-11-18 15:58:19 -0500

    Updated to export the bulkio interface as well as fixed the exports for IDM and ODM since some objects weren't exported

--- src/sockets/sockets.module.ts
+++ src/sockets/sockets.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 
-export { basicSocket, WebSocketBinaryType } from './basic.socket';
-export { EventChannelService } from './event.channel.service';
-export * from './odm/odm.listener.service';
-export * from './idm/idm.listener.service';
+// BULKIO service and data types
+export { BulkioListenerService }from './bulkio/bulkio.listener.service';
+export { BulkioPacket } from './bulkio/bulkio.packet';
+export { SRI } from './bulkio/sri';
+export { PrecisionUTCTime } from './bulkio/precision.utc.time';
 
-export { RhMessage } from './message/message';
+
+// Event and derived services and types
+export { EventChannelService } from './event.channel.service';
+export { OdmListenerService } from './odm/odm.listener.service';
+export { IdmListenerService } from './idm/idm.listener.service';
+export { OdmEvent, SourceCategory }from './odm/odm.event';
+export * from './idm/idm.event';
+export * from './message/message';
 
 @NgModule({/** INTENTIONALLY EMPTY */})
 export class SocketsModule {}
