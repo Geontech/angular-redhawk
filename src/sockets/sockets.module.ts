0dea7dbe54568263e9d36d9a103de80229a19f3f
Thomas Goodwin <btgoodwin@geontech.com>
2017-01-03 17:14:48 -0500

    Roll-up of recent changes to the top-level sockets files including exporting the new pipes, etc.

--- src/sockets/sockets.module.ts
+++ src/sockets/sockets.module.ts
@@ -10,5 +10,36 @@ export * from './idm/idm.event';
 
 export { RhMessage } from './message/message';
 
-@NgModule({/** INTENTIONALLY EMPTY */})
+// Pipes for enumerations
+import {
+    AdministrativeStatePipe,
+    OperationalStatePipe,
+    UsageStatePipe
+} from './idm/idm.event.pipes';
+
+import {
+    SourceCategoryPipe,
+    ResourceStateChangePipe
+} from './odm/odm.event.pipes';
+
+@NgModule({
+    exports: [
+        // IDM
+        AdministrativeStatePipe,
+        OperationalStatePipe,
+        UsageStatePipe,
+        // ODM
+        SourceCategoryPipe,
+        ResourceStateChangePipe
+    ],
+    declarations: [
+        // IDM
+        AdministrativeStatePipe,
+        OperationalStatePipe,
+        UsageStatePipe,
+        // ODM
+        SourceCategoryPipe,
+        ResourceStateChangePipe
+    ]
+})
 export class SocketsModule {}
