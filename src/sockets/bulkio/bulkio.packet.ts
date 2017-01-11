a0b358366bfa361fe213aea9052d66c0bb705535
Thomas Goodwin <btgoodwin@geontech.com>
2017-01-11 16:37:07 -0500

    Migrated to the enumeration rather than a string type, as we've done in many other REST-facing areas where we can support it

--- src/sockets/bulkio/bulkio.packet.ts
+++ src/sockets/bulkio/bulkio.packet.ts
@@ -3,15 +3,40 @@ import { ISerializable } from '../../shared/serializable';
 import { SRI } from './sri';
 import { PrecisionUTCTime } from './precision.utc.time';
 
-export type BulkioDataType = 
-    'dataChar' |
-    'dataShort' |
-    'dataLong' |
-    'dataLongLong' |
-    'dataULong' |
-    'dataULongLong' |
-    'dataFloat' |
-    'dataDouble';
+export enum BulkioDataType {
+    dataChar,
+    dataShort,
+    dataLong,
+    dataLongLong,
+    dataULong,
+    dataULongLong,
+    dataFloat,
+    dataDouble,
+    UNKNOWN
+}
+
+export function resolve(dataType: string): BulkioDataType {
+    switch (dataType) {
+        case 'dataChar':
+            return BulkioDataType.dataChar;
+        case 'dataShort':
+            return BulkioDataType.dataShort;
+        case 'dataLong':
+            return BulkioDataType.dataLong;
+        case 'dataLongLong':
+            return BulkioDataType.dataLongLong;
+        case 'dataULong' :
+            return BulkioDataType.dataULong;
+        case 'dataULongLong' :
+            return BulkioDataType.dataULongLong;
+        case 'dataFloat' :
+            return BulkioDataType.dataFloat;
+        case 'dataDouble' :
+            return BulkioDataType.dataDouble;
+        default:
+            return BulkioDataType.UNKNOWN;
+    }
+}
 
 export class BulkioPacket implements ISerializable<BulkioPacket> {
     streamID: string;
@@ -23,12 +48,12 @@ export class BulkioPacket implements ISerializable<BulkioPacket> {
     dataBuffer: Array<number>;
 
     deserialize(input: any) {
-        this.streamID
+        this.streamID = input.streamID;
         this.T = new PrecisionUTCTime().deserialize(input.T);
         this.EOS = input.EOS;
         this.sriChanged = input.sriChanged;
         this.SRI = new SRI().deserialize(input.SRI);
-        this.type = input.type;
+        this.type = resolve(input.type);
         this.dataBuffer = input.dataBuffer;
         return this;
     }
