5c2692ed84acfcb052c08907522b4dc28571489a
Thomas Goodwin <btgoodwin@geontech.com>
2017-10-24 12:36:27 -0400

    Restored device commands back to the angular module -- this was an error earlier

--- src/models/device/index.ts
+++ src/models/device/index.ts
@@ -2,6 +2,3 @@
  * REDHAWK Device models and interfaces
  */
 export * from './device';
-export * from './device-property-command';
-export * from './device-property-command-response';
-export * from './device-property-command-type';
