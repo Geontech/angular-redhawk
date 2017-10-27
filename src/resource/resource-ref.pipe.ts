6248ea6c203a3a284af7ee16d390b64ab6fd99c9
Thomas Goodwin <btgoodwin@geontech.com>
2017-10-27 14:18:26 -0400

    Added regex support to both, added key support to arResourceRef

--- src/resource/resource-ref.pipe.ts
+++ src/resource/resource-ref.pipe.ts
@@ -7,13 +7,21 @@ import { IRefFilter } from './ref-filter';
 /**
  * Returns a specific ResourceRef by its ID or Name out of a list matching first
  * the ID if provided, otherwise the name is used to match.
+ *
+ * Supports Regular Expressions.
+ *
+ * You can specify the 'key' argument to return just the string member matching
+ * that key (in this case, 'id' or 'name' would work).
  */
 @Pipe({name: 'arResourceRef'})
 export class ResourceRefPipe implements PipeTransform {
-    transform(refs: ResourceRefs, target: IRefFilter): ResourceRef {
-        return refs.filter(r => {
-            return ((target.id !== undefined) && (r.id === target.id)) ||
-                ((target.name !== undefined) && (r.name === target.name));
-        })[0] || undefined;
+    transform(refs: ResourceRefs, target: IRefFilter, key?: string): ResourceRef | string {     
+        let id   = new RegExp(target.id || '.*');
+        let name = new RegExp(target.name || '.*');
+        let ref = refs.filter(r => { return (id.test(r.id) || name.test(r.name)); })[0] || undefined;
+        if (ref !== undefined && key !== undefined) {
+            return ref[key]; // Returns the key.
+        }
+        return ref;
     }
 }
--- src/resource/resource-refs.pipe.ts
+++ src/resource/resource-refs.pipe.ts
@@ -7,13 +7,16 @@ import { IRefFilter } from './ref-filter';
 /**
  * Returns a list (ResourceRefs) that match either the ID or the Name specified
  * in the filter.
+ *
+ * Supports Regular Expressions.
  */
 @Pipe({name: 'arResourceRefs'})
 export class ResourceRefsPipe implements PipeTransform {
     transform(refs: ResourceRefs, target: IRefFilter): ResourceRefs {
+        let id   = new RegExp(target.id || '.*');
+        let name = new RegExp(target.name || '.*');
         return refs.filter(r => {
-            return ((target.id !== undefined) && (r.id === target.id)) ||
-                ((target.name !== undefined) && (r.name === target.name));
+            return (id.test(r.id) || name.test(r.name));
         });
     }
 }
