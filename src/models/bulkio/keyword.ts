abea51eda4536a4c5b1d8ebde6d6daac77a6a1fd
btgoodwin <btgoodwin@geontech.com>
2018-02-07 15:25:59 -0500

    Converted keywords to an actual Map
    Implementation of deserialize function did not actually create a
    real Map object; now it does.

--- src/models/bulkio/keyword.ts
+++ src/models/bulkio/keyword.ts
@@ -1,13 +1,20 @@
 import { ISerializableFn } from '../serialization/index';
 
 /**
- * Keywords are just a map, basically.
+ * Keywords are basic JS object, however we're converting
+ * it to a Map to avoid concerns about object member names.
  */
 export type Keywords = Map<string, any>;
 
 let deserializeKeywords: ISerializableFn<Keywords>;
 deserializeKeywords = function (inputs?: any): Keywords {
-    return <Keywords> inputs;
+    let keys: Keywords = new Map<string, any>();
+    if (inputs) {
+        Object.keys(inputs).forEach(key => {
+            keys.set(key, inputs[key]);
+        });
+    }
+    return keys;
 };
 
 export { deserializeKeywords }
